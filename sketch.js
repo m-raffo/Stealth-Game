2bd5845dee62a43562bb5b4f3ea09fe601ae1812
unknown <masonraffo@icloud.com>
2019-02-05 17:39:03 -0500

    Add objects for camera and items, improve image display functions

--- sketch.js
+++ sketch.js
@@ -1,31 +1,91 @@
-function setup() {
-  createCanvas( window.innerWidth , window.innerHeight);
-
-
+var canvas;
+var guy = {
+  // image : loadImage("guy.png"),
+  x : 0,
+  y : 0,
+  width: 100,
+  height : 100
 }
 
-var camera = {
+var cam = {
   x : 0,
   y : 0,
   width : 500,
-  height : 500
+  height : 500,
+
+  onScreen : function(pointX, pointY) {
+    if (pointX < this.x || pointY < this.y || pointX > this.x + width || pointY > this.y + height){
+      return false;
+    } else {
+      return true;
+    }
+  },
+
+  // returns the x coord of the upper left point of the camera
+  originX : function() {
+    return this.x - (width/2);
+  },
+  originY : function() {
+    return this.y - (height/2);
+  }
+
+
 }
+
+function setup() {
+  canvas = createCanvas(windowWidth,windowHeight);
+
+  // hide scroll bars
+  document.documentElement.style.overflow = 'hidden';  // firefox, chrome
+  document.body.scroll = "no"; // ie only
+
+  guy.image = loadImage("guy.png");
+
+}
+
+
 
 function draw() {
   background("#363636");
+
+  drawItem(guy);
+
+
+
 }
 
 
 
-// Takes in an object with x and y variables, uses camera variable
+// Takes in an item with x and y variables, uses cam variable
 // display it in the correct location onscreen
-function drawObject(object) {
+function drawItem(item) {
 
-  // if offscreen
-  if (object.x - (object.width / 2) < camera.x - (camera.width / 2) ||
-    object.x + (object.width / 2) > camera.x + (camera.width / 2) ||
-    object.y - (object.height / 2) < camera.y - (camera.height / 2) ||
-    object.y + (object.height / 2) > camera.y + (camera.height / 2)) {
+  // if offscreen, return null
+  if (cam.onScreen(item.x - (item.width/2), item.y - (item.height/2)) ||
+      cam.onScreen(item.x + (item.width/2), item.y - (item.height/2)) ||
+      cam.onScreen(item.x - (item.width/2), item.y + (item.height/2)) ||
+      cam.onScreen(item.x + (item.width/2), item.y + (item.height/2)) ) {
+
+
+      // these variables are not totally necessay, created to improve readability
+      var imageWidth = item.width;
+      var imageHeight = item.height;
+
+
+      console.log("Drawing image!!!");
+      console.log(item.x - cam.originX());
+      console.log();
+
+
+      image(item.image, (item.x - cam.originX()) - (imageWidth / 2), (item.y - cam.originY()) - (imageWidth / 2), imageWidth, imageHeight);
+
+  } else {
+    console.log("Offscreen");
     return null;
   }
+
 }
+
+window.onresize = function() {
+  resizeCanvas(windowWidth, windowHeight)
+};
