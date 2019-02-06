1eae6495b5387373d6c9ecda67a11978d1aa611b
unknown <masonraffo@icloud.com>
2019-02-06 17:32:11 -0500

    Add docs

--- cam.js
+++ cam.js
@@ -4,7 +4,12 @@ var cam = {
   width : 2000,
   height : 2000,
 
-  // determines if the given point is on screen
+  /**
+   * Determines if a point is on screen
+   * @param  {Number} pointX x coordinate of the point
+   * @param  {Number} pointY y coordinate of the point
+   * @return {Boolean}        True if on the screen, False if not
+   */
   onScreen : function(pointX, pointY) {
     if (pointX < this.originX() || pointY < this.originY() || pointX > this.originX() + this.width || pointY > this.originY() + this.height){
       return false;
@@ -13,7 +18,11 @@ var cam = {
     }
   },
 
-  // determines if the object is on the screen
+  /**
+   * Determines if the object if on the onScreen
+   * @param  {Object} item  the item to be checked
+   * @return {Boolean}      True if onscreen, False if not
+   */
   objectOnScreen : function(item) {
 
     if (this.onScreen(item.x - (item.width/2), item.y - (item.height/2)) ||
@@ -26,10 +35,20 @@ var cam = {
         }
   },
 
-  // returns the x coord of the upper left point of the camera
+  /**
+   * Returns the x coordinate in the world of the upper-left corner of the
+   *   camera
+   * @return {Number} x coordinate
+   */
   originX : function() {
     return this.x - (this.width/2);
   },
+
+  /**
+   * Returns the y coordinate in the world of the upper-left corner of the
+   *   camera
+   * @return {Number} y coordinate
+   */
   originY : function() {
     return this.y - (this.height/2);
   }
