85d6cb1c6854bd62d6792a979f7d1d5e5c422ead
unknown <masonraffo@icloud.com>
2019-02-06 17:16:44 -0500

    Add objectOnScreen(); fix origin calculations

--- cam.js
+++ cam.js
@@ -1,23 +1,37 @@
 var cam = {
   x : 0,
   y : 0,
-  width : 500,
-  height : 500,
+  width : 2000,
+  height : 2000,
 
+  // determines if the given point is on screen
   onScreen : function(pointX, pointY) {
-    if (pointX < this.x || pointY < this.y || pointX > this.x + width || pointY > this.y + height){
+    if (pointX < this.originX() || pointY < this.originY() || pointX > this.originX() + this.width || pointY > this.originY() + this.height){
       return false;
     } else {
       return true;
     }
   },
 
+  // determines if the object is on the screen
+  objectOnScreen : function(item) {
+
+    if (this.onScreen(item.x - (item.width/2), item.y - (item.height/2)) ||
+        this.onScreen(item.x + (item.width/2), item.y - (item.height/2)) ||
+        this.onScreen(item.x - (item.width/2), item.y + (item.height/2)) ||
+        this.onScreen(item.x + (item.width/2), item.y + (item.height/2)) ) {
+          return true;
+        } else {
+          return false;
+        }
+  },
+
   // returns the x coord of the upper left point of the camera
   originX : function() {
-    return this.x - (width/2);
+    return this.x - (this.width/2);
   },
   originY : function() {
-    return this.y - (height/2);
+    return this.y - (this.height/2);
   }
 
 
