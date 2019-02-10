a1dc0c8d4917cebdc4a8fcb06ef797443da6db27
Mason739 <masonraffo@icloud.com>
2019-02-10 10:16:05 -0500

    Add eslint style checking

--- src/controls.js
+++ src/controls.js
@@ -4,18 +4,18 @@
  *   system
  * @type {Object}
  */
-var controls = {
+const controls = {
   /**
    * Contains the key bindings for all of the controls of the game
    * @type {Object}
    */
-  bindings : {
-    "MOVE_UP" : [38, 87],    // up arrow and 'w' key
-    "MOVE_LEFT" : [37, 65],  // left arrow and 'a' key
-    "MOVE_RIGHT" : [39, 68], // right arrow and 'd' key
-    "MOVE_DOWN" : [40, 83],  // down arrow and 'd' key
-    "CROUCH" : [16],         // shift
-    "SPRINT" : [32],         // space
+  bindings: {
+    'MOVE_UP': [38, 87], // up arrow and 'w' key
+    'MOVE_LEFT': [37, 65], // left arrow and 'a' key
+    'MOVE_RIGHT': [39, 68], // right arrow and 'd' key
+    'MOVE_DOWN': [40, 83], // down arrow and 'd' key
+    'CROUCH': [16], // shift
+    'SPRINT': [32], // space
   },
   /**
    * Checks if the given control is currently pressed.
@@ -23,12 +23,12 @@ var controls = {
    * @param  {String} control  the name of the control (as a string)
    * @return {Boolean}         True if the key is pressed, False if it is not
    */
-  isControlPressed : function( control ) {
+  isControlPressed: function( control ) {
     if (this.bindings.hasOwnProperty(control)) {
-      // List of all the potential options for keys to press to activate the control
-      var options = this.bindings[control];
+      // List of all the options for keys to press to activate the control
+      const options = this.bindings[control];
 
-      for (var i = 0; i < options.length; i++) {
+      for (let i = 0; i < options.length; i++) {
         if (keyIsDown(options[i])) {
           return true;
         }
@@ -37,25 +37,24 @@ var controls = {
       // if a matching control has not been found after looping through the
       // entire list, return false
       return false;
-
-    } else {  // if the control does not have a binding, return false
+    } else { // if the control does not have a binding, return false
       return false;
     }
   },
 
   /**
-   * Logs all of the controls and if they are currently pressed or not 
+   * Logs all of the controls and if they are currently pressed or not
    * (For debugging)
    * @return {undefined} No return value
    */
-  logControls : function() {
+  logControls: function() {
+    console.log('====CURRENT CONTROLS===========================');
 
-    console.log("====CURRENT CONTROLS===========================");
-
-    Object.keys(this.bindings).forEach(key => {
-
-      console.log("    " + key.toString() + "  " + controls.isControlPressed(key).toString());
+    Object.keys(this.bindings).forEach((key) => {
+      console.log('    ' + key.toString() + '  ' +
+        controls.isControlPressed(key).toString());
     });
-    console.log("===============================================")
-  }
+
+    console.log('===============================================');
+  },
 };
