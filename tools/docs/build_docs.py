140d754c1a4cffd3bcb3b1968c8ff502b97dff42
Mason739 <masonraffo@icloud.com>
2019-02-10 11:15:34 -0500

    Update to properly rename created docs website to 'docs-site'

--- tools/docs/build_docs.py
+++ tools/docs/build_docs.py
@@ -7,6 +7,9 @@ for file in os.listdir("./"):
     if file.endswith(".js") and file != "p5.js":
         os.system("jsdoc2md {0} >> {0}.md".format(file))
         shutil.move("{0}.md".format(file), "../docs/{0}.md".format(file))
+
 os.chdir('../')
 os.system("mkdocs build")
+if os.path.isdir('docs-site'):
+    shutil.rmtree('docs-site')
 shutil.move('site', 'docs-site')
