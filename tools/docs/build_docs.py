9db42a08f3cc8b2f29381482900c2f775e2a2d32
Mason739 <>
2019-02-11 10:03:31 -0500

    Updated to use npx

--- tools/docs/build_docs.py
+++ tools/docs/build_docs.py
@@ -5,11 +5,12 @@ import shutil
 os.chdir("../../src")
 for file in os.listdir("./"):
     if file.endswith(".js") and file != "p5.js":
-        os.system("jsdoc2md {0} >> {0}.md".format(file))
+        os.system("npx jsdoc2md {0} >> {0}.md".format(file))
         shutil.move("{0}.md".format(file), "../docs/{0}.md".format(file))
 
 os.chdir('../')
-os.system("mkdocs build")
+# TODO: make it use the correct install of python
+os.system("python3 -m mkdocs build")
 if os.path.isdir('docs-site'):
     shutil.rmtree('docs-site')
 shutil.move('site', 'docs-site')
